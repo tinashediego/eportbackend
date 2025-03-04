6d2de0d5d05a51e48f3b854de8204cabe50dcb35
tinashediego <you@example.com>
2025-03-04 19:19:26 +0200

    user viewset

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def register(request):
+    """ Register a new user """
+    email = request.data.get('email')
+    username = request.data.get('username')
+    password = request.data.get('password')
+    role = request.data.get('role', 'clerk')  # Default role is 'clerk'
+
+    if not email or not username or not password:
+        return Response({'error': 'Missing required fields'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(email=email, username=username, password=password, role=role)
+    return Response({'tokens': user.tokens(), 'user': UserSerializer(user).data}, status=status.HTTP_201_CREATED)
