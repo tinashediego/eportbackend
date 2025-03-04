a0d027dbd02b81fb4a920252502ff10d562f4578
tinashediego <you@example.com>
2025-03-04 19:14:56 +0200

    user model with 2 roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('clerk', 'Clerk'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='clerk')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'role']
